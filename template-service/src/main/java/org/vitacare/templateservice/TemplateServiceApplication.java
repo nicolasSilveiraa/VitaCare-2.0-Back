b5df0e896ef6ad342ef668ccfe3f59ca8b068712
MATHC <92028037+Math23Kskb@users.noreply.github.com>
2025-07-02 18:01:22 -0300

    build: add template-service as a blueprint for business microservices

---
+++ template-service/src/main/java/org/vitacare/templateservice/TemplateServiceApplication.java
@@ -0,0 +1,13 @@
+package org.vitacare.templateservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+class TemplateServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TemplateServiceApplication.class, args);
+    }
+
+}
