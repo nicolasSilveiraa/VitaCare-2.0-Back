344d77f6f59164fa232d3b7c8f4d1bb3bc96f005
MATHC <92028037+Math23Kskb@users.noreply.github.com>
2025-07-02 18:01:08 -0300

    feat(infra): add initial modules for gateway, config, and registry

---
+++ config-server/src/main/java/org/vitacare/configserver/ConfigServerApplication.java
@@ -0,0 +1,15 @@
+package org.vitacare.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+class ConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+
+}
