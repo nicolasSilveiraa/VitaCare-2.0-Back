f2ffbd517a40d5d5fcafe8ce7224352fbe728ffd
Your Name <you@example.com>
2025-07-05 18:06:05 -0300

    Fix

--- patient-service/src/main/java/org/vitacare/repository/PacienteRepository.java
+++ patient-service/src/main/java/org/vitacare/repository/PacienteRepository.java
@@ -8,6 +8,5 @@ import org.vitacare.model.PacienteModel;
 public interface PacienteRepository extends JpaRepository<PacienteModel, Long> {
 
     Boolean existsByNomePaciente(String nome);
-    Boolean existsByCpfPaciente(String cpfPaciente);
 
 }
