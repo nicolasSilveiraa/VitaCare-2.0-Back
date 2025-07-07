878d58d969d9f0723772c639017f3bc8e5f02df7
mathc <mathchiyuki@gmail.com>
2025-07-07 15:21:49 -0300

    chore: Remove módulo template-service e refatora config-server

--- template-service/src/main/resources/data.sql
+++
@@ -1,9 +0,0 @@
--- Este script insere dados de teste na tabela 'templates'
--- O schema é 'templates', então o nome completo é 'templates.templates'
-
--- Apaga dados antigos para garantir um início limpo a cada execução
-DELETE FROM templates.templates;
-
--- Insere os novos templates de teste
-INSERT INTO templates.templates (name, content) VALUES ('Email de Boas-Vindas', 'Olá {{userName}}, seja bem-vindo!');
-INSERT INTO templates.templates (name, content) VALUES ('Notificação de Consulta', 'Sua consulta está agendada para {{appointmentDate}}.');
