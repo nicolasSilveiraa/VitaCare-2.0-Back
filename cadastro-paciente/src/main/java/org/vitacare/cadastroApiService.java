586b877c5198990a1959abb6757f07ca5f7ef440
Your Name <you@example.com>
2025-07-14 23:06:25 -0300

    Criação cadastro Paciente

---
+++ cadastro-paciente/src/main/java/org/vitacare/cadastroApiService.java
@@ -0,0 +1,13 @@
+package org.vitacare;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class cadastroApiService {
+    public static void main(String[] args) {
+        SpringApplication.run(cadastroApiService.class, args);
+    }
+}
