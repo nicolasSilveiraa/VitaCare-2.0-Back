e25c9349fc3236db37c5e8e24d6c1d6cb82db71f
Your Name <you@example.com>
2025-07-15 22:04:58 -0300

    Fix

---
+++ cadastro-paciente/src/main/java/org/vitacare/dto/response/PlanoResponse.java
@@ -0,0 +1,14 @@
+package org.vitacare.dto.response;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class PlanoResponse {
+
+    private Long id;
+    private String nome;
+    private String tipo;
+
+}
---
+++ cadastro-paciente/src/main/java/org/vitacare/repository/ConvenioClient.java
@@ -0,0 +1,8 @@
+package org.vitacare.repository;
+
+
+import org.springframework.cloud.openfeign.FeignClient;
+
+@FeignClient
+public interface ConvenioClient {
+}
